7f34eb67d578fc55ae82124e8b36326fb5234371
Carlos Rubio <carlos.rr@chihuahua2.tecnm.mx>
2024-03-12 15:24:45 -0600

    Clase 12 marzo

--- mi_primera_app/models.py
+++ mi_primera_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    #Definir los campos que tiene la tabla
+    top_name = models.CharField(max_length=100,unique=True)
+
+    #Representar el registro como cadena de texto
+    def __str__(self):
+        return self.top_name
+    
+class Webpage(models.Model):
+    #Definir los campos que tiene la tabla
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+    url = models.URLField(unique=True)
+
+     #Representar el registro como cadena de texto
+    def __str__(self):
+        return self.name
+    
+class AccessRecord(models.Model):
+    webname = models.ForeignKey(Webpage, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    #Representar el registro como cadena de texto
+    def __str__(self):
+        return str(self.date)
