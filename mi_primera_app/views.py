e06f213cb32e4203cd8f2d1e475ce287bbae471d
Carlos Rubio <carlos.rr@chihuahua2.tecnm.mx>
2024-03-14 14:55:17 -0600

    Clase 14 marzo

--- mi_primera_app/views.py
+++ mi_primera_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mi_primera_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(request):
-    my_context = {'username': 'Hola desde views.py'}
+    access_list = AccessRecord.objects.order_by('date')
+    my_context = {'username': 'Hola desde views.py',
+                  'access_records': access_list}
     return render(request, 'mi_primera_app/index.html', context=my_context)
     #return HttpResponse("<h1>Recuerdo el día en que de la chamba yo me enamoré</h1>")
