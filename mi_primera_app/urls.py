9f92e11ae5eb28542dba984597fd89a020303862
Carlos Rubio <carlos.rr@chihuahua2.tecnm.mx>
2024-04-09 14:49:54 -0600

    Clase 9 abril

--- mi_primera_app/urls.py
+++ mi_primera_app/urls.py
@@ -3,5 +3,6 @@ from mi_primera_app import views
 
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('formpage/', views.form_user_view, name='form_user')
 ]
--- mi_primera_app/views.py
+++ mi_primera_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from mi_primera_app.models import Topic, Webpage, AccessRecord, ElProfe
+from . import forms
 
 # Create your views here.
 def index(request):
@@ -11,3 +12,8 @@ def index(request):
                   'el_profe': elprofe_list}
     return render(request, 'mi_primera_app/index.html', context=my_context)
     #return HttpResponse("<h1>Recuerdo el día en que de la chamba yo me enamoré</h1>")
+
+#Crear un formulario para mostrar
+def form_user_view(request):
+    form = forms.FormUser()
+    return render(request, 'mi_primera_app/form_page.html', {'form' : form})
